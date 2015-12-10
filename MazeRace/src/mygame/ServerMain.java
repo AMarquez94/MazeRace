562bc7144442c643e88981dbeb6aeeb19b5f19f0
AMarquez94 <alejandromarquez94@gmail.com>
2015-12-10 14:55:15 +0100

    Testing server

--- MazeRace/src/mygame/ServerMain.java
+++ MazeRace/src/mygame/ServerMain.java
@@ -1,34 +1,60 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
-import com.jme3.material.Material;
+import com.jme3.app.StatsAppState;
+import com.jme3.bullet.BulletAppState;
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.light.AmbientLight;
 import com.jme3.math.ColorRGBA;
-import com.jme3.math.Vector3f;
+import com.jme3.network.HostedConnection;
+import com.jme3.network.Network;
+import com.jme3.network.Server;
 import com.jme3.renderer.RenderManager;
-import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
+import com.jme3.terrain.geomipmap.TerrainLodControl;
+import com.jme3.terrain.geomipmap.TerrainQuad;
+import com.jme3.terrain.heightmap.AbstractHeightMap;
+import com.jme3.terrain.heightmap.ImageBasedHeightMap;
+import com.jme3.texture.Texture;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * test
  * @author normenhansen
  */
 public class ServerMain extends SimpleApplication {
-
+    
+    private Server server;
+    private HostedConnection[] hostedConnections;
+    
+    private TerrainQuad terrain;
+    
+    private BulletAppState bas;
+    
     public static void main(String[] args) {
+        Networking.initialiseSerializables();
         ServerMain app = new ServerMain();
         app.start();
+    }
+    
+    public ServerMain(){
+        super(new StatsAppState());
     }
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
-
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        
+        try {
+            server = Network.createServer(Networking.PORT);
+            server.start();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerMain.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.pauseOnFocus = false;
+        
+        setUpWorld();
+        setUpLight();
     }
 
     @Override
@@ -39,5 +65,43 @@ public class ServerMain extends SimpleApplication {
     @Override
     public void simpleRender(RenderManager rm) {
         //TODO: add render code
+    }
+    
+    private void setUpWorld() {
+
+        Texture heightMapImage = assetManager.loadTexture("Textures/Heightmap1.png");
+        AbstractHeightMap heightmap = new ImageBasedHeightMap(heightMapImage.getImage());
+        heightmap.load();
+
+        /**
+         * 3. We have prepared material and heightmap. Now we create the actual
+         * terrain: 3.1) Create a TerrainQuad and name it "my terrain". 3.2) A
+         * good value for terrain tiles is 64x64 -- so we supply 64+1=65. 3.3)
+         * We prepared a heightmap of size 512x512 -- so we supply 512+1=513.
+         * 3.4) As LOD step scale we supply Vector3f(1,1,1). 3.5) We supply the
+         * prepared heightmap itself.
+         */
+        int patchSize = 65;
+        terrain = new TerrainQuad("Maze", patchSize, 257, heightmap.getHeightMap());
+        
+        terrain.setLocalTranslation(0, -100, 0);
+        terrain.setLocalScale(2f, 0.5f, 2f);
+
+        terrain.addControl(new RigidBodyControl(0));
+        bas.getPhysicsSpace().add(terrain);
+
+        TerrainLodControl control = new TerrainLodControl(terrain, getCamera());
+        terrain.addControl(control);
+
+        rootNode.attachChild(terrain);
+    }
+    
+    
+    
+     private void setUpLight() {
+        // We add light so we see the scene
+        AmbientLight al = new AmbientLight();
+        al.setColor(ColorRGBA.White.mult(5f));
+        rootNode.addLight(al);
     }
 }
